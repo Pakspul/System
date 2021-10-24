4bf77462a10ed31e5b62b627a9d3d87d676ba568
Pakspul <pakspul@gmail.com>
2021-10-24 10:04:15 +0200

    NotImplementedException message added

--- src/System/NotImplementedException.php
+++ src/System/NotImplementedException.php
@@ -4,4 +4,10 @@ namespace System;
 
 class NotImplementedException extends \Exception
 {
+    public function __construct($message)
+    {
+        $message = $message ?? 'The method or operation is not implemented';
+
+        parent::__construct($message);
+    }
 }
